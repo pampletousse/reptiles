cea4153bf8d1632ed5fd1318d6927df98eb6d536
pampletousse <sloppin23@gmail.com>
2020-01-17 16:54:56 +0100

    feat(reptiles_app):Ajout de classe Reptile

--- reptiles_app/models.py
+++ reptiles_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 # Create your models here.
+class Reptile(models.Model):
+    nom = models.CharField(max_length=100)
+    nomEspece = models.CharField(max_length=200)
+    ordre = models.CharField(max_length=200)
+    poids = models.PositiveIntegerField()
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+

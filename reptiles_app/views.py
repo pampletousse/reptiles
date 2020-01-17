728da8f72eb94d449d1b34c04793e57b0e58dff1
pampletousse <sloppin23@gmail.com>
2020-01-17 16:39:54 +0100

    feat(reptiles):Ajout d'une page d'accueil et mise en place de l'url de urls.py pour reptiles_app

--- reptiles_app/views.py
+++ reptiles_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    template = loader.get_template("reptiles/home.html")
+    context = {
+        "homeText":"Accueil",
+    }
+    return HttpResponse(template.render(context,request))

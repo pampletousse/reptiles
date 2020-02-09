8cb99c65178bae00758e2c28de9f356a2008e151
pampletousse <sloppin23@gmail.com>
2020-02-09 22:00:17 +0100

    feat(reptiles_app): Création d'un encart header pour gestion active du user + modification des templates + ajouts de liens

--- reptiles_app/views.py
+++ reptiles_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,get_object_or_404,redirect
 from django.template import loader
 from django.http import HttpResponse
-from .models import Reptile
+from .models import Reptile,User
 from .forms import ReptileModelForm
 from django.contrib.auth import authenticate,logout,login
 from django.contrib.auth.decorators import login_required
@@ -11,9 +11,7 @@ from django.contrib.auth.decorators import login_required
 def home(request):
 
     template = loader.get_template("reptiles/home.html")
-    context = {
-        "homeText":"Accueil",
-    }
+    context = getUserContext(request)
     return HttpResponse(template.render(context,request))
 
 @login_required(login_url='/connexion/')
@@ -22,45 +20,57 @@ def create(request):
     if request.method == 'GET':
         template = loader.get_template("reptiles/formCreate.html")
         form = ReptileModelForm()
-        context = {"form":form,
-        "typeEnvoi":"create",}
-        print(request.META.get('HTTP_REFERER'))
+        context = getUserContext(request)
+        context["form"]=form
+        context["typeEnvoi"]="create"
+        return HttpResponse(template.render(context,request))
+
     elif request.method == 'POST':
         template = loader.get_template("reptiles/formCreate.html")
-        context={"form":ReptileModelForm(request.POST),
-        "typeEnvoi":"create"}
+        context = getUserContext(request)
+        context["form"]=ReptileModelForm(request.POST)
+        context["typeEnvoi"]="create"
+
         if context["form"].is_valid():
             s = context["form"].save()
+        return redirect("/liste/")
         
-    return HttpResponse(template.render(context,request))
+    
 
 @login_required(login_url='/connexion/')
 def update(request,pk):
-
     reptile = get_object_or_404(Reptile,pk=pk)
     form = ReptileModelForm(request.POST or None, instance=reptile)
     template = loader.get_template("reptiles/formUpdate.html")
-    context = {"form":form,"typeEnvoi":"update","pk":pk}
-    if form.is_valid():
-        s = form.save()
+    context = getUserContext(request)
+    context['form']=form
+    context['typeEnvoi']='update'
+    context['pk']=pk
 
-    return HttpResponse(template.render(context,request))
+    if request.method == 'GET':
+        return HttpResponse(template.render(context,request))
+
+    elif request.method == 'POST':
+        if form.is_valid():
+            s = form.save()
+
+        return redirect("/liste/")
 
 @login_required(login_url='/connexion/')
 def delete(request,pk):
 
     template = loader.get_template("reptiles/liste.html")
     reptile = get_object_or_404(Reptile,pk=pk)
-    context = {}
+    context = getUserContext(request)
     Reptile.objects.filter(pk=pk).delete()
 
-    return HttpResponse(template.render(context,request))
+    return redirect("/liste/")
 
 def liste(request):
-    print(request.user)
     template = loader.get_template("reptiles/liste.html")
     reptiles = Reptile.objects.all()
-    context = {"reptiles":reptiles}
+    context = getUserContext(request)
+    context['reptiles'] = reptiles
 
     return HttpResponse(template.render(context,request))
 
@@ -68,9 +78,9 @@ def detail(request,pk):
 
     template = loader.get_template("reptiles/detail.html")
     reptile = get_object_or_404(Reptile,pk=pk)
-    context = {
-        "reptile":reptile
-    }
+    context = getUserContext(request)
+    context['reptile']=reptile
+
     return HttpResponse(template.render(context,request))
 
 def connexion(request):
@@ -82,7 +92,6 @@ def connexion(request):
             login(request, user)
             return redirect("/liste/")
         else:
-            print("non connecte")
             template = loader.get_template("reptiles/connexion.html")
             context = {}
 
@@ -90,6 +99,20 @@ def connexion(request):
 
 def deconnexion(request):
     logout(request)
-    template = loader.get_template("reptiles/connexion.html")
-    context = {}
+    template = loader.get_template("reptiles/home.html")
+    context = getUserContext(request)
     return HttpResponse(template.render(context,request))
+
+def getUserContext(request):
+    context={}
+    if request.user.is_authenticated == False:
+        nom = ""
+        codeco = "connexion"
+    else:
+        nom = request.user.username
+        codeco = "deconnexion"
+    context["userContext"]=nom
+    context["codeco"]=codeco
+
+    return context
+    
